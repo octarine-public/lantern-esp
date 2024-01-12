9367090268944a0739166af62b88973ac3405c56
SicdeX <sicdex@gmail.com>
2024-01-13 05:30:11 +0600

    see desc
    - add vision radius

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import "./translations"
 import {
 	DOTAGameState,
 	DOTAGameUIState,
+	Entity,
 	EventsSDK,
 	GameRules,
 	GameState,
-	Modifier
+	Lantern,
+	Modifier,
+	ParticlesSDK
 } from "github.com/octarine-public/wrapper/index"
 
 import { LanternGUI } from "./gui"
@@ -15,6 +18,7 @@ import { MenuManager } from "./menu"
 const bootstrap = new (class CLanternESP {
 	private readonly gui = new LanternGUI()
 	private readonly menu = new MenuManager()
+	private readonly pSDK = new ParticlesSDK()
 
 	private readonly modifiers: Modifier[] = []
 	private readonly buffNames = ["modifier_lamp_on", "modifier_lamp_off"]
@@ -52,6 +56,10 @@ const bootstrap = new (class CLanternESP {
 		}
 	}
 
+	public Tick() {
+		/** @todo */
+	}
+
 	public ModifierCreated(modifier: Modifier) {
 		if (this.buffNames.includes(modifier.Name)) {
 			this.modifiers.push(modifier)
@@ -64,12 +72,32 @@ const bootstrap = new (class CLanternESP {
 		}
 	}
 
+	public EntityTeamChanged(entity: Entity) {
+		if (!(entity instanceof Lantern)) {
+			return
+		}
+		console.log("EntityTeamChanged", entity)
+	}
+
+	public EntityDestroyed(entity: Entity) {
+		if (!(entity instanceof Lantern)) {
+			return
+		}
+		console.log("EntityDestroyed", entity)
+	}
+
 	public GameChanged() {
 		this.menu.GameChanged()
+	}
+
+	protected isLantern(entity: Entity) {
+		return entity instanceof Lantern
 	}
 })()
 
 EventsSDK.on("Draw", () => bootstrap.Draw())
+
+EventsSDK.on("Tick", () => bootstrap.Tick())
 
 EventsSDK.on("GameEnded", () => bootstrap.GameChanged())
 
@@ -78,3 +106,7 @@ EventsSDK.on("GameStarted", () => bootstrap.GameChanged())
 EventsSDK.on("ModifierCreated", modifier => bootstrap.ModifierCreated(modifier))
 
 EventsSDK.on("ModifierRemoved", modifier => bootstrap.ModifierRemoved(modifier))
+
+EventsSDK.on("EntityDestroyed", entity => bootstrap.EntityTeamChanged(entity))
+
+EventsSDK.on("EntityTeamChanged", entity => bootstrap.EntityTeamChanged(entity))
